6eb869dbb889953d2860bf0f2262b8aff76a500c
Agus <aarduino@udesa.edu.ar>
2024-05-17 11:02:21 -0300

    ej 1 y 2

---
+++ ej1/js/precalentado.js
@@ -0,0 +1,10 @@
+
+
+let mensaje = "Vas a salir de casa?"
+let resultado = confirm(mensaje)
+if (resultado == true){
+    alert("Chequeá el pronóstico del tiempo antes de salir")
+}
+else{ alert("Que bueno que te quedes. Va a llover mucho hoy.")
+}
+
---
+++ ej2/js/experimento.js
@@ -0,0 +1,28 @@
+
+
+let mensaje = "Ingresa tu nombre"
+let nombre = prompt(mensaje)
+console.log(nombre)
+
+let mensaje2 = "Ingresa tu edad"
+let edad = prompt(mensaje2)
+console.log(edad)
+
+let mensaje3 = "Te gustan los deportes?"
+let fanDeportes = confirm(mensaje3)
+console.log(fanDeportes)
+
+
+let total = alert(`Muchas Gracias ${nombre} por responder a nuestras preguntas`)
+
+let usuario ={
+    nombre = nombre
+    edad = edad
+    deportistaProfesional: function(){
+        if (fanDeportes){
+            console.log("Si, soy fan de los deportes")
+        }
+        else{"No soy tan fan aún de los deportes"
+        }
+    }
+}
